fd5ce0d17e474330dd94d49e511ab609abb7cab3
m4rk-log <143257867+m4rk-log@users.noreply.github.com>
2023-08-28 14:12:23 +0100

    Add files via upload

---
+++ rtl8192eu-dongle.sh
@@ -0,0 +1,10 @@
+#update,upgrade,autoremove
+sudo apt update -y && apt upgrade -y && apt autremove -y 
+
+#linux headers 
+sudo apt-get install git linux-headers-generic build-essential dkms -y && git clone https://github.com/Mange/rtl8192eu-linux-driver
+cd rtl8192eu-linux-driver && sudo dkms add . && sudo dkms install rtl8192eu/1.0 
+#echo blacklisting 
+echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf && echo -e "8192eu\n\nloop" | sudo tee /etc/modules && echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
+sudo update-grub; sudo update-initramfs -u
+
