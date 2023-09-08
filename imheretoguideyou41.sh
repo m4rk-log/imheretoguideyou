8145dea0aec3ecd27c76577fab6c7a636bd7a8bd
m4rk-log <143257867+m4rk-log@users.noreply.github.com>
2023-09-08 16:48:11 -0400

    Update and rename imheretoguideyou40.sh to imheretoguideyou41.sh

--- imheretoguideyou41.sh
+++ imheretoguideyou41.sh
@@ -8,3 +8,6 @@ apt install firmware-b43-installer firmware-b43legacy-installer -y
 # ocs-url,flatpak, softwares
 apt install libqt5svg5 qml-module-qtquick-controls htop neofetch stacer gparted lightdm-gtk-greeter-settings -y
 apt install flatpak -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#modules for ssh and more 
+sudo apt install git python3 python3-pip php openssh-client -y 
+
